ca917f16fd4dc533fc21c0b5ab02cf01627f9ed6
Patricia <46973349+Patco96@users.noreply.github.com>
2025-04-04 13:09:29 -0300

    Workflow graph

--- src/graph.py
+++ src/graph.py
@@ -11,17 +11,26 @@ from src.edges import tools_condition, grade_documents
 workflow = StateGraph(AgentState)
 
 # Define the nodes we will cycle between
-...
+workflow.add_node("agent", agent)
+retrieve = ToolNode([retriever_tool])
+workflow.add_node("retrieve", retrieve)
+workflow.add_node("rewrite", rewrite)
+workflow.add_node("generate", generate)
 
 # Call agent node to decide to retrieve or not
-...
+workflow.add_edge(START, "agent")
 
 # Decide whether to retrieve
-...
+workflow.add_conditional_edges(
+    "agent",
+    tools_condition,
+    {"tools": "retrieve", END: END},
+)
 
 # Edges taken after the `action` node is called.
-...
+workflow.add_conditional_edges("retrieve", grade_documents)
+workflow.add_edge("generate", END)
+workflow.add_edge("rewrite", "agent")
 
 # Compile
-
 graph = workflow.compile()
